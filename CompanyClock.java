b795d0378226e1cb2bb6e15a25d642df691b73da
Charles Shapleigh <CShapleigh@gmail.com>
2015-10-26 19:11:45 -0400

    initialized company clock time

--- CompanyClock.java
+++ CompanyClock.java
@@ -1,6 +1,10 @@
 public class CompanyClock extends Thread {
   protected static int currentTime;
 
+  public CompanyClock() {
+    currentTime = 0;
+  }
+
   public void run() {
     while(true) {
       CompanyClock.currentTime++;
